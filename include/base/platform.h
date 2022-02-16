5076406cc098f39d056d9d4a440e4e90a2630fa9
Rasmus Rendal <rasmus@rend.al>
2022-02-16 10:35:23 +0100

    Remove unneeded code from base

--- include/base/platform.h
+++ include/base/platform.h
@@ -31,29 +31,6 @@
 extern "C" {
 #endif
 
-/** strptime is not available under Windows
- * so we have an implementation of it,
- * otherwise include <time.h> to get the
- * standard (posix) version under Linux.
- */
-char* strptime2(const char* buf, const char* fmt, struct tm* tm, time_t* now);
-
-/** Finer grain time measure function.
- * @return number of CPU cycles (TSC asm call).
- * This is officially implemented only starting with Intel Pentium II platforms.
- * Unreliable due to power saving (hibernation, freq scaling), multi-cpu/core.
- */
-static inline uint64_t gettsc(void)
-{
-#if defined(__GNUG__) && defined(i386)
-    uint32_t lo, hi;
-    asm volatile("rdtsc" : "=a"(lo), "=d"(hi));
-    return (uint64_t)hi << 32 | lo;
-#else
-    return 0;
-#endif
-}
-
 /**
  * Returns an OS-specific error description for given error code.
  * (this is a replacement for strerror which is inconsistent
@@ -86,23 +63,6 @@ void base_getProcInfo(procinfo_t*);
 /** Gets the current process memory and time consumption sample and stores the maximum (peak)
  * values. */
 void base_getProcInfoMax(procinfo_t*);
-
-/* MAC addresses. */
-
-typedef uint8_t mac_adr_t[8];
-
-typedef struct
-{
-    size_t size;
-    mac_adr_t mac[];
-} maclist_t;
-
-/**
- * Get the available MAC addresses. The
- * structure is malloc allocated and should
- * be freed. Returns NULL in case of error.
- */
-maclist_t* base_getMAC();
 
 #ifdef __cplusplus
 }
