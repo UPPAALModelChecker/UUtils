4909dd2b2398001e9ee6cf0c6b86a36d61ec1919
Marius Mikučionis <marius@cs.aau.dk>
2023-08-29 10:30:47 +0200

    Updated getlibs script and added compile.sh to build automatically for various targets

---
+++ compile.sh
@@ -0,0 +1,55 @@
+#/usr/bin/env bash
+set -e
+
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+for target in "$@" ; do
+    BUILD_TYPE=""
+    BUILD_TARGET=""
+    BUILD_SUFFIX=""
+    CMAKE_TOOLCHAIN_FILE=""
+    BUILD_TOOLCHAIN=""
+    BUILD_PREFIX=""
+    BULD_EXTRA=""
+    case "$target" in
+        linux64*)
+            BUILD_TARGET=linux64
+            ;;
+        macos*)
+            BUILD_TARGET=macos64
+            ;;
+        win64*)
+            BUILD_TARGET=win64
+            CMAKE_TOOLCHAIN_FILE="$PROJECT_DIR/toolchains/mingw.cmake"
+            BUILD_TOOLCHAIN="-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}"
+            ;;
+        *)
+            echo "Failed to recognize target platform: $target"
+            exit 1
+            ;;
+    esac
+    case "$target" in
+        *lib*)
+            BUILD_PREFIX="-DCMAKE_PREFIX_PATH=$PROJECT_DIR/libs-$BUILD_TARGET"
+            BUILD_SUFFIX="-libs"
+            CMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN_FILE" "$PROJECT_DIR/getlibs.sh" "$BUILD_TARGET"
+            ;;
+    esac
+    case "$target" in
+        *debug)
+            BUILD_TYPE=Debug
+            ;;
+        *release)
+            BUILD_TYPE=Release
+            BUILD_EXTRA="-DUUtils_WITH_BENCHMARKS=ON"
+            ;;
+        *)
+            echo "Failed to recognize build type: $target"
+            echo "Using Debug instead"
+            BUILD_TYPE=Debug
+    esac
+    BUILD_DIR="build-${BUILD_TARGET}${BUILD_SUFFIX}-${BUILD_TYPE,,}"
+    cmake -B "$BUILD_DIR" "$BUILD_PREFIX" "$BUILD_TOOLCHAIN" "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" $BUILD_EXTRA -S "$PROJECT_DIR"
+    cmake --build "$BUILD_DIR"
+    (cd "$BUILD_DIR" ; ctest --output-on-failure)
+done
--- getlibs.sh
+++ getlibs.sh
@@ -2,68 +2,92 @@
 #set -euxo pipefail
 set -euo pipefail
 
-CMAKE_ARGS="-DCMAKE_BUILD_TYPE=Release"
-if [ -z ${CMAKE_TOOLCHAIN_FILE+x} ]; then
-	echo "Not using a custom toolchain";
-else
-	echo "Using toolchain $CMAKE_TOOLCHAIN_FILE";
-	CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE";
-fi
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Cursed line that should also work on macos
-# https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
-SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-SOURCES="$SOURCE_DIR/libs/sources"
-mkdir -p "$SOURCES"
+for target in "$@" ; do
+    BUILD_TARGET=$target
 
-## XXHASH
-SOURCE="$SOURCES/xxHash-0.8.2"
-BUILD="$SOURCE/build"
-cd "$SOURCES"
-if [ ! -r "xxhash-0.8.2.tgz" ]; then
-  wget "https://github.com/Cyan4973/xxHash/archive/refs/tags/v0.8.2.tar.gz" -cO xxhash-0.8.2.tgz
-fi
-if [ ! -d "$SOURCE" ] ; then
-  tar xf "xxhash-0.8.2.tgz"
-fi
-mkdir -p "$BUILD"
-cmake -S "$SOURCE/cmake_unofficial" -B "$BUILD" $CMAKE_ARGS -DCMAKE_INSTALL_PREFIX="$SOURCE_DIR/libs" -DBUILD_SHARED_LIBS=OFF
-cmake --build "$BUILD" --config Release
-cmake --install "$BUILD" --config Release
-rm -Rf "$SOURCE"
+    CMAKE_ARGS="-DCMAKE_BUILD_TYPE=Release"
+    if [ -z ${CMAKE_TOOLCHAIN_FILE+x} ]; then
+	    echo "Not using a custom toolchain";
+    else
+	    echo "Using toolchain $CMAKE_TOOLCHAIN_FILE";
+	    CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TOOLCHAIN_FILE";
+    fi
 
-## DOCTEST
-SOURCE="$SOURCES/doctest-2.4.11"
-BUILD="$SOURCE/build"
-cd "$SOURCES"
-if [ ! -r "doctest-2.4.11.tgz" ]; then
-  wget "https://github.com/doctest/doctest/archive/refs/tags/v2.4.11.tar.gz" -cO doctest-2.4.11.tgz
-fi
-if [ ! -d "${SOURCE}" ]; then
-  tar xf "doctest-2.4.11.tgz"
-fi
-cmake -S "$SOURCE" -B "$BUILD" $CMAKE_ARGS -DCMAKE_INSTALL_PREFIX="$SOURCE_DIR/libs" -DDOCTEST_WITH_TESTS=OFF -DDOCTEST_WITH_MAIN_IN_STATIC_LIB=ON -DCMAKE_BUILD_TYPE=Release
-cmake --build "$BUILD" --config Release
-cmake --install "$BUILD" --config Release
-rm -Rf "$SOURCE"
+    SOURCE_DIR="$PROJECT_DIR/libs-sources"
+    mkdir -p "$SOURCE_DIR"
+    PREFIX_DIR="$PROJECT_DIR/libs-$BUILD_TARGET"
 
-## BOOST
-PACK="boost-1.83.0.tar.xz"
-SOURCE="$SOURCES/boost-1.83.0"
-BUILD="$SOURCE/build"
-cd "$SOURCES"
-if [ ! -r "$PACK" ]; then
-#  wget "https://github.com/boostorg/boost/releases/download/{$PACK%.tar.xz}/$PACK" -cO "$PACK"
-  wget "https://homes.cs.aau.dk/~marius/mirrors/boost/$PACK" -cO "$PACK"
-fi
-if [ ! -d "$SOURCE" ]; then
-  tar xf "$PACK"
-fi
-mkdir -p "$BUILD"
-cmake -S "$SOURCE" -B "$BUILD" $CMAKE_ARGS -DCMAKE_INSTALL_PREFIX="$SOURCE_DIR/libs" -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DBOOST_INCLUDE_LIBRARIES="headers;math" -DBOOST_ENABLE_MPI=OFF -DBOOST_ENABLE_PYTHON=OFF -DBOOST_RUNTIME_LINK=static -DBUILD_TESTING=OFF -DBOOST_USE_STATIC_LIBS=ON -DBOOST_USE_DEBUG_LIBS=ON -DBOOST_USE_RELEASE_LIBS=ON -DBOOST_USE_STATIC_RUNTIME=ON
-cmake --build "$BUILD" --config Release
-cmake --install "$BUILD" --config Release
-rm -Rf "$SOURCE"
+    ## XXHASH
+    LIBRARY="xxHash-0.8.2"
+    VERSION=${LIBRARY/[^-]*-/}
+    SOURCE="$SOURCE_DIR/$LIBRARY"
+    BUILD="$PREFIX_DIR/tmp/$LIBRARY"
+    if [ -r "$PREFIX_DIR/include/xxhash.h" ]; then
+        echo "$LIBRARY is already installed in $PREFIX_DIR"
+    else
+        echo "Building $LIBRARY in $BUILD from $SOURCE"
+        pushd "$SOURCE_DIR"
+        if [ ! -r "$LIBRARY.tgz" ]; then
+            wget "https://github.com/Cyan4973/xxHash/archive/refs/tags/v$VERSION.tar.gz" -cO "$LIBRARY.tgz"
+        fi
+        if [ ! -d "$SOURCE" ] ; then
+            tar xf "$LIBRARY.tgz"
+        fi
+        popd
+        mkdir -p "$BUILD"
+        cmake -S "$SOURCE/cmake_unofficial" -B "$BUILD" $CMAKE_ARGS -DCMAKE_INSTALL_PREFIX="$PREFIX_DIR" -DBUILD_SHARED_LIBS=OFF
+        cmake --build "$BUILD" --config Release
+        cmake --install "$BUILD" --config Release
+        rm -Rf "$BUILD"
+    fi
 
-#mkdir -p "$SOURCE_DIR/libs/boost/include"
-#cp -Rp boost_1_83_0/boost "$SOURCE_DIR/libs/boost/include"
+    ## DOCTEST
+    LIBRARY="doctest-2.4.11"
+    VERSION=${LIBRARY/[^-]*-/}
+    SOURCE="$SOURCE_DIR/$LIBRARY"
+    BUILD="$PREFIX_DIR/tmp/$LIBRARY"
+    if [ -r "$PREFIX_DIR/include/doctest/doctest.h" ]; then
+        echo "$LIBRARY is already installed in $PREFIX_DIR"
+    else
+        echo "Building $LIBRARY in $BUILD from $SOURCE"
+        pushd "$SOURCE_DIR"
+        if [ ! -r "$LIBRARY.tgz" ]; then
+            wget "https://github.com/doctest/doctest/archive/refs/tags/v$VERSION.tar.gz" -cO "$LIBRARY.tgz"
+        fi
+        if [ ! -d "${SOURCE}" ]; then
+            tar xf "$LIBRARY.tgz"
+        fi
+        popd
+        cmake -S "$SOURCE" -B "$BUILD" $CMAKE_ARGS -DCMAKE_INSTALL_PREFIX="$PREFIX_DIR" -DDOCTEST_WITH_TESTS=OFF -DDOCTEST_WITH_MAIN_IN_STATIC_LIB=ON
+        cmake --build "$BUILD" --config Release
+        cmake --install "$BUILD" --config Release
+        rm -Rf "$BUILD"
+    fi
+
+    ## BOOST
+    LIBRARY="boost-1.83.0"
+    VERSION=${LIBRARY/[^-]*-/}
+    SOURCE="$SOURCE_DIR/$LIBRARY"
+    BUILD="$PREFIX_DIR/tmp/$LIBRARY"
+    if [ -r "$PREFIX_DIR/include/boost/math/distributions/arcsine.hpp" ] || [ "$PREFIX_DIR/include/boost-1_83/boost/math/distributions/arcsine.hpp" ] ; then
+        echo "$LIBRARY is already installed in $PREFIX_DIR"
+    else
+        echo "Building $LIBRARY in $BUILD from $SOURCE"
+        pushd "$SOURCE_DIR"
+        if [ ! -r "$LIBRARY.tar.xz" ]; then
+            #  wget "https://github.com/boostorg/boost/releases/download/{$PACK%.tar.xz}/$PACK" -cO "$PACK"
+            wget "https://homes.cs.aau.dk/~marius/mirrors/boost/$LIBRARY.tar.xz" -cO "$LIBRARY.tar.xz"
+        fi
+        if [ ! -d "$SOURCE" ]; then
+            tar xf "$LIBRARY.tar.xz"
+        fi
+        mkdir -p "$BUILD"
+        cmake -S "$SOURCE" -B "$BUILD" $CMAKE_ARGS -DCMAKE_INSTALL_PREFIX="$PREFIX_DIR" -DBUILD_SHARED_LIBS=OFF -DBOOST_INCLUDE_LIBRARIES="headers;math" -DBOOST_ENABLE_MPI=OFF -DBOOST_ENABLE_PYTHON=OFF -DBOOST_RUNTIME_LINK=static -DBUILD_TESTING=OFF -DBOOST_USE_STATIC_LIBS=ON -DBOOST_USE_DEBUG_LIBS=ON -DBOOST_USE_RELEASE_LIBS=ON -DBOOST_USE_STATIC_RUNTIME=ON
+        cmake --build "$BUILD" --config Release
+        cmake --install "$BUILD" --config Release
+        rm -Rf "$BUILD"
+    fi
+done
+echo "Success!"
