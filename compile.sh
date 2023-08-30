26e30c08dc9f38690b30b922d73a7c3d552a7ce2
Marius Mikučionis <marius@cs.aau.dk>
2023-08-30 14:06:21 +0200

    Fixed and cleaned compile.sh

--- compile.sh
+++ compile.sh
@@ -18,14 +18,12 @@ fi
 PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for target in "$@" ; do
-    echo "COMPILE for $target"
-    BUILD_TYPE=""
-    BUILD_TARGET=""
-    BUILD_SUFFIX=""
-    CMAKE_TOOLCHAIN_FILE=""
-    BUILD_TOOLCHAIN=""
-    BUILD_PREFIX=""
-    BULD_EXTRA=""
+    unset CMAKE_TOOLCHAIN_FILE
+    unset CMAKE_PREFIX_PATH
+    unset CMAKE_BUILD_TYPE
+    unset BUILD_TARGET
+    unset BUILD_SUFFIX
+    unset BUILD_EXTRA
     case "$target" in
         linux64*)
             BUILD_TARGET=linux64
@@ -35,8 +33,7 @@ for target in "$@" ; do
             ;;
         win64*)
             BUILD_TARGET=win64
-            CMAKE_TOOLCHAIN_FILE="$PROJECT_DIR/toolchains/mingw.cmake"
-            BUILD_TOOLCHAIN="-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}"
+            export CMAKE_TOOLCHAIN_FILE="$PROJECT_DIR/toolchains/mingw.cmake"
 	    if [ -z "$WINEPATH" ]; then
 		export WINEPATH=$("$PROJECT_DIR/mingw-winepath.sh")
 	    fi
@@ -48,32 +45,32 @@ for target in "$@" ; do
     esac
     case "$target" in
         *lib*)
-            BUILD_PREFIX="-DCMAKE_PREFIX_PATH=$PROJECT_DIR/libs-$BUILD_TARGET"
+	    CMAKE_BUILD_TYPE=Release "$PROJECT_DIR/getlibs.sh" "$BUILD_TARGET"
+            export CMAKE_PREFIX_PATH="$PROJECT_DIR/libs-$BUILD_TARGET"
             BUILD_SUFFIX="-libs"
-	    if [ -n "$CMAKE_TOOLCHAIN_FILE" ]; then
-		CMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN_FILE" CMAKE_BUILD_TYPE=Release "$PROJECT_DIR/getlibs.sh" "$BUILD_TARGET"
-	    else
-		CMAKE_BUILD_TYPE=Release "$PROJECT_DIR/getlibs.sh" "$BUILD_TARGET"
-	    fi
 	    BUILD_EXTRA="-DFIND_FATAL=ON ${BUILD_EXTRA}"
             ;;
     esac
     case "$target" in
         *debug)
-            BUILD_TYPE=Debug
+            export CMAKE_BUILD_TYPE=Debug
             ;;
         *release)
-            BUILD_TYPE=Release
+            export CMAKE_BUILD_TYPE=Release
             BUILD_EXTRA="-DUUtils_WITH_BENCHMARKS=ON $BUILD_EXTRA"
             ;;
         *)
             echo "Failed to recognize build type: $target"
             echo "Using Debug instead"
-            BUILD_TYPE=Debug
+            export CMAKE_BUILD_TYPE=Debug
     esac
-    BUILD_DIR="build-${BUILD_TARGET}${BUILD_SUFFIX}-${BUILD_TYPE,,}"
-    cmake -B "$BUILD_DIR" "$BUILD_PREFIX" "$BUILD_TOOLCHAIN" -DCMAKE_BUILD_TYPE=$BUILD_TYPE $BUILD_EXTRA -S "$PROJECT_DIR"
-    cmake --build "$BUILD_DIR" --config $BUILD_TYPE
-    (cd "$BUILD_DIR" ; ctest -C $BUILD_TYPE --output-on-failure)
+    BUILD_DIR="build-${BUILD_TARGET}${BUILD_SUFFIX}-${CMAKE_BUILD_TYPE,,}"
+    echo "COMPILE for $target in $BUILD_DIR using $BUILD_EXTRA"
+    echo "  CMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE:-(unset)}"
+    echo "  CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH:-(unset)}"
+    echo "  CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-(unset)}"
+    cmake -S "$PROJECT_DIR" -B "$BUILD_DIR" $BUILD_EXTRA
+    cmake --build "$BUILD_DIR" --config $CMAKE_BUILD_TYPE
+    (cd "$BUILD_DIR" ; ctest -C $CMAKE_BUILD_TYPE --output-on-failure)
     echo "COMPILE $target success!"
 done
